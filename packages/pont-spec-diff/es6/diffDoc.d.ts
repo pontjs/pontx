ef8625a3dd357373d4b2236982502e89ba9fc2d5
jasonHzq <hzongquan@126.com>
2022-06-20 11:51:35 +0800

    fix: .gitignore is working now

--- packages/pont-spec-diff/es6/diffDoc.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class BaseClazzDiffOp {
-    static getFields(fields: string[]): string;
-    static getSchemaDiffItems(diffResult: any, fields: string[]): {
-        localValue: any;
-        remoteValue: any;
-        fields: string[];
-        type: string;
-    };
-}
--- packages/pont-spec/es6/parameter.d.ts
+++
@@ -1,7 +0,0 @@
-import { PontJsonSchema } from "./dataType";
-export declare class Parameter {
-    schema: PontJsonSchema;
-    name: string;
-    in: "query" | "body" | "path" | "formData" | "header";
-    required: boolean;
-}
