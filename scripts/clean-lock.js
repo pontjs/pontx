9a15d687dc36038f14507d42a68546df9ca096f4
jasonHzq <hzongquan@126.com>
2023-09-14 12:00:00 +0800

    chore: publish

--- scripts/clean-lock.js
+++ scripts/clean-lock.js
@@ -1,6 +1,7 @@
 const child_process = require("child_process");
 
 try {
+  child_process.execSync("rm package-lock.json");
   child_process.execSync("rm packages/**/package-lock.json");
   child_process.execSync("rm plugins/**/package-lock.json");
 } catch (e) {}
