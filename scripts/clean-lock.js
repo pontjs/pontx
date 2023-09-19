2ab2a900a05e206445270e329699a4f553cbe53c
jasonHzq <hzongquan@126.com>
2023-09-19 14:05:47 +0800

    feat: code

--- scripts/clean-lock.js
+++ scripts/clean-lock.js
@@ -1,13 +1,30 @@
 const child_process = require("child_process");
+const path = require("path");
 
 try {
-  child_process.execSync("rm package-lock.json");
+  child_process.execSync("rm package-lock.json", {
+    cwd: path.join(__dirname + "../../"),
+    stdio: [0, 1, 2],
+  });
 } catch (e) {}
 
 try {
-  child_process.execSync("rm packages/**/package-lock.json");
+  child_process.execSync("rm packages/**/package-lock.json", {
+    cwd: path.join(__dirname + "../../"),
+    stdio: [0, 1, 2],
+  });
 } catch (e) {}
 
 try {
-  child_process.execSync("rm plugins/**/package-lock.json");
+  child_process.execSync("rm plugins/**/package-lock.json", {
+    cwd: path.join(__dirname + "../../"),
+    stdio: [0, 1, 2],
+  });
+} catch (e) {}
+
+try {
+  child_process.execSync("rm plugins/**/*/package-lock.json", {
+    cwd: path.join(__dirname + "../../"),
+    stdio: [0, 1, 2],
+  });
 } catch (e) {}
