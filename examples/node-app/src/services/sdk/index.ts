eb3b801dabad859c2d6dbe0564b2fb232c047c8f
jasonHzq <hzongquan@126.com>
2023-12-19 11:37:40 +0800

    fix: fix code

---
+++ examples/node-app/src/services/sdk/index.ts
@@ -0,0 +1,11 @@
+/**
+ * This file is the default generated SDK usage.
+ * You can also implement and expose your custom pontx usage outside of pontx outDir.
+ */
+import * as API from './api';
+import * as defs from './defs';
+
+export {
+	API,
+	defs
+};
